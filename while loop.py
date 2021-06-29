1e71139e4c1eeeab31097e0fff297f920e87422f
shivhudda <shivhudda072@gmail.com>
2021-06-29 16:14:27 +0530

    this is first file uploading by me

---
+++ while loop.py
@@ -0,0 +1,23 @@
+i = 1
+while i<7:
+    i+=1
+    if i>3:
+        break
+    print(i)
+
+i = 1
+while i<7:
+    i+=1
+    if i<3:
+        continue
+    print(i)
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+    
+
+
